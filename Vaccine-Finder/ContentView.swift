398c38edd07b985af36fb65c82904652a205049c
Lisa Sam Wang <sam_wang_lisa1121@berkeley.edu>
2021-02-14 07:35:47 -0800

    Initial Commit

---
+++ Vaccine-Finder/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Vaccine-Finder
+//
+//  Created by Lisa Sam Wang on 2/14/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Vaccine-Finder/Vaccine_FinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  Vaccine_FinderApp.swift
+//  Vaccine-Finder
+//
+//  Created by Lisa Sam Wang on 2/14/21.
+//
+
+import SwiftUI
+
+@main
+struct Vaccine_FinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
